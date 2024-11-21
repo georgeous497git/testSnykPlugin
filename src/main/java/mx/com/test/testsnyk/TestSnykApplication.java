a89925627d02111591684b2252fd78b4a9fd919b
georgeous497 <georgeous497@gmail.com>
2024-11-21 11:40:46 -0600

    Initial commit

---
+++ src/main/java/mx/com/test/testsnyk/TestSnykApplication.java
@@ -0,0 +1,13 @@
+package mx.com.test.testsnyk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSnykApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSnykApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mx/com/test/testsnyk/TestSnykApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.test.testsnyk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSnykApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
